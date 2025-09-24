5ed307df4d71b7fd1d15e0065732f04eb3659857
cglotr <aaryfn@gmail.com>
2025-09-23 23:24:16 -0700

    minor

--- pfp__impl.go
+++ pfp__impl.go
@@ -14,7 +14,7 @@ func (p *Pfp__impl) Resize(img image.Image) (image.Image, error) {
 		sz = img.Bounds().Dy()
 	}
 	rect := image.Rect(0, 0, sz, sz)
-	img__crop := image.NewRGBA(image.Rect(0, 0, sz, sz))
+	img__crop := image.NewRGBA(rect)
 	for y := 0; y < sz; y++ {
 		for x := 0; x < sz; x++ {
 			img__crop.Set(x, y, img.At(x, y))
